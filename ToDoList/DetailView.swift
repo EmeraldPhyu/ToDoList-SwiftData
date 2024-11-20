11b986354b7f4cf83b7726f5506c9123a3f0e490
Emerald <phyuhninhtway@gmail.com>
2024-11-20 17:01:18 +0800

    Amendment update

--- ToDoList/DetailView.swift
+++ ToDoList/DetailView.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct DetailView: View {
+	var passedValue: String // Don't initialize it - it will passed from the parent view
+	@Environment(\.dismiss) private var dismiss
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+			VStack{
+				Image(systemName: "swift")
+					.resizable()
+					.scaledToFit()
+					.foregroundStyle(.orange)
+					Text("You are Swifty Legend\n And you are passed over the value : \(passedValue)")
+					.font(.largeTitle)
+					.multilineTextAlignment(.center)
+				Spacer()
+				Button("Get Back!") {
+					dismiss()
+				}
+			}.padding()
+		}
 }
 
 #Preview {
-    DetailView()
+	DetailView(passedValue: "")
 }
--- ToDoList/ToDoListView.swift
+++ ToDoList/ToDoListView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  ToDoListView.swift
 //  ToDoList
 //
 //  Created by Emerald on 20/11/24.
@@ -8,17 +8,24 @@
 import SwiftUI
 
 struct ToDoListView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	var toDos = ["Learn Swift", "Build Apps", "Learn to play new song", "Keep calm and Paint", "Go a Date", "Workout", "Meditate"]
+	var body: some View {
+		NavigationStack {
+			List{
+				ForEach(toDos, id: \.self) { toDo in
+					NavigationLink {
+						DetailView(passedValue: toDo)
+					} label: {
+						Text(toDo)
+					}
+				}
+			}.navigationTitle("To Do List")
+				.navigationBarTitleDisplayMode(.automatic)
+				.listStyle(.plain)
+		}
+	}
 }
 
 #Preview {
-    ToDoListView()
+	ToDoListView()
 }
